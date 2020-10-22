cd800d869206cc8a57b05188a19d10d534357d9d
Emma James <ejames@companieshouse.gov.uk>
2020-10-22 22:55:20 +0100

    Updating the layout to include the menu for account pages

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,20 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+
+
+// routing for DIY filer directors to the end of the jounrey
+// routing directors that use agents to enter presenter details
+router.post('/who-files-for-the-company', function (req, res) {
+ 
+    if(req.session.data['who-files'] == "no"){
+
+      res.redirect('interruption-card-before-account')
+    }
+    else if(req.session.data['who-files'] == "yes"){
+
+      res.redirect('/delegate-to-presenter/enter-presenter-details')
+    }
+  
+})
