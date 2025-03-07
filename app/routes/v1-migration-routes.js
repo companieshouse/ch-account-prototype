0c2246713068a363d7d039824fb34ef0bf68d73f
Emma <ejames@companieshouse.gov.uk>
2025-03-07 16:28:38 +0000

    webfiling screener flow added into task 2

--- app/routes/v1-migration-routes.js
+++ app/routes/v1-migration-routes.js
@@ -19,6 +19,59 @@ router.post('/migration-1/prototype-set-up', function (req, res) {
 /* Start page for sign in */
 router.post('/migration-1/start-page', function (req, res) {
 
+
+    if (req.session.data['set-journey'].includes('WC2')) {
+       
+        res.redirect('sign-in-webfiling-interupt-simplified')
+    }
+    else    
+    {
+           
+        res.redirect('sign-in-interupt')
+    }
+
+
+    
+})
+
+/* Start page for sign in */
+router.post('/migration-1/sign-in-webfiling-interupt', function (req, res) {
+
+    res.redirect('one-login/create-or-sign-in')
+})
+
+
+/* Start page for sign in screen flow */
+router.post('/migration-1/sign-in-webfiling-interupt-simplified', function (req, res) {
+
+    res.redirect('account-support')
+})
+
+/* Start page for sign in screen flow */
+router.post('/migration-1/account-support', function (req, res) {
+
+    if (req.session.data['mutliple-accounts'].includes('yes')) {
+       
+        res.redirect('account-support-1')
+    }
+    else    
+    {
+           
+        res.redirect('account-support-2')
+    }
+
+})
+
+
+/* Start page for sign in screen flow */
+router.post('/migration-1/account-support-1', function (req, res) {
+
+    res.redirect('one-login/create-or-sign-in')
+})
+
+/* Start page for sign in screen flow */
+router.post('/migration-1/account-support-2', function (req, res) {
+
     res.redirect('one-login/create-or-sign-in')
 })
 
@@ -29,6 +82,8 @@ router.post('/migration-1/one-login/one-login-sign-in', function (req, res) {
 
     res.redirect('one-login/one-login-enter-password')
 })
+
+
 router.post('one-login/one-login-enter-password', function (req, res) {
 
     res.redirect('enter-code')
@@ -41,7 +96,7 @@ router.post('/migration-1/one-login/enter-code', function (req, res) {
 
     if (req.session.data['set-journey'].includes('WC1')) {
        
-        res.redirect('../verify-email-address')
+        res.redirect('../webfiling-interupt')
     }
     else if (req.session.data['set-journey'].includes('WC2'))     
     {
@@ -57,23 +112,28 @@ router.post('/migration-1/one-login/enter-code', function (req, res) {
 })
 
 
+/* Webfiling interupt */
+router.post('/migration-1/webfiling-interupt', function (req, res) {
+
+    res.redirect('verify-email-address')
+})
+
+
+
+/* Webfiling interupt */
+router.post('/migration-1/verify-email-address', function (req, res) {
+
+    res.redirect('end-linking')
+})
+
+
+
+
 /* Bringing the user back to Companies House from GOV.UK One Login */
 router.post('/migration-1/end-linking', function (req, res) {
 
-
-  if (req.session.data['set-journey'].includes('WC1')) {
+    res.redirect('company-overview')
        
-
-  }
-  else if (req.session.data['set-journey'].includes('WC2'))   {
-       
-
-  }
-  else   {
-    
-    res.redirect('create-or-sign-in')
-  }
-
 })
 
 
@@ -99,43 +159,19 @@ router.post('/migration-1/no-chs-found-interrupt', function (req, res) {
     //no, continue to WebFiling 
 
 
-    if (req.session.data['no-chs-found'].includes('no')){
+    //if your on task 3 
+    if (req.session.data['no-chs-found'].includes('yes')){
 
-        if (req.session.data['set-journey'].includes('WC3')){
+        res.redirect('no-chs-found-change-email')
+          
+    }
+    else if (req.session.data['no-chs-found'].includes('no')) {
 
-            res.redirect('no-webfiling-found')
-        }  
-       
+        res.redirect('no-webfiling-found')
         
     }
-    else if (req.session.data['no-chs-found'].includes('later')){
-
-        if (req.session.data['set-journey'].includes('WC3')){
-
-            res.redirect('no-webfiling-found')
-        }  
-       
-        
-    }
-    else if (req.session.data['no-chs-found'].includes('yes')) {
-
-        if (req.session.data['set-journey'].includes('WC3')){
-
-            res.redirect('no-chs-found-change-email')
-        }  
-       
-        
-    }
-    else if (req.session.data['set-journey'].includes('WC2'))     
-    {
-           
-        res.redirect('../link-to-existing-chs-account')
-    }
 
 
-    //yes, continue to CHS sign in
-
-    res.redirect('verify-email-address')
 
 })
 
@@ -150,26 +186,18 @@ router.post('/migration-1/no-webfiling-found', function (req, res) {
 
     if (req.session.data['no-webfiling-found'].includes('no')){
 
-        if (req.session.data['set-journey'].includes('WC3')){
-
-            res.redirect('webfiling-account-home')
-        }  
-       
-        
+        res.redirect('company-overview')
+         
     }
     else if (req.session.data['no-webfiling-found'].includes('yes')) {
-
-        if (req.session.data['set-journey'].includes('WC3')){
-
-            res.redirect('no-webfiling-found-change-email')
-        }  
-       
         
+        res.redirect('no-webfiling-found-change-email')
+    
     }
 
     //yes, continue to CHS sign in
 
-    res.redirect('verify-email-address')
+  //  res.redirect('verify-email-address')
 
 })
 
