aaef86db8e3a4f794bea96e0a7f12324f4bfcb13
hwarrenCH <hwarren@companieshouse.gov.uk>
2025-04-30 15:02:44 +0100

    Version 4 created

--- app/routes.js
+++ app/routes.js
@@ -19,3 +19,5 @@ router.use('/', require('./routes/v2-migration-routes.js'))
 router.use('/', require('./routes/v2a-migration-routes.js'))
 
 router.use('/', require('./routes/v3-migration-routes.js'))
+
+router.use('/', require('./routes/v4-migration-routes.js'))
